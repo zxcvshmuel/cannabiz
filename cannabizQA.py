aebf911e89d2d9d96d78d0dc9e5c543eaecbb84c
Shmuel Adler <43316601+zxcvshmuel@users.noreply.github.com>
2024-09-01 14:54:15 +0300

    Update cannabizQA.py

--- cannabizQA.py
+++ cannabizQA.py
@@ -6,7 +6,8 @@ import os
 import re
 
 # Set up Google Gemini AI
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure("env:GOOGLE_API_KEY")
 model = genai.GenerativeModel('gemini-pro')
 
 # Function to get column names from the database
